5c2620ab2251a3da810ffc798413c917561d0116
Blarshdeep <91928572+blarsh24@users.noreply.github.com>
2022-08-17 21:38:38 -0700

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,32 @@
+import csv
+from datetime import date
+
+dt = date.today()
+dt = dt.strftime("%m/%d/%y")
+filename = "expenses.csv"
+
+costs = []
+total = 0
+status = True
+
+with open(filename, "a", newline="") as file:
+    csvwriter = csv.writer(file)
+    while status == True:
+
+        expense = float(input("How much did you spend? Enter 0 to exit. "))
+
+        costs.append(expense)
+
+        if expense == 0:
+            costs.pop()
+            total = round(sum(costs), 2)
+            average = round(total / len(costs), 2)
+            print(f"The total you spent today was: {total}")
+            print(f"Your average transaction total today was: {average}")
+            status = False
+        else:
+            csvwriter.writerow([dt, expense])
+
+file.close()
+
+print("test")_
